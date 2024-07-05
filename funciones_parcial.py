f7935f192ac28a53b7cb4b4d43deed4267173a5c
MarceloPoloni <marcelopoloni2@gmail.com>
2024-07-05 08:11:30 -0300

    parcial modificado

--- funciones_parcial.py
+++ funciones_parcial.py
@@ -6,109 +6,336 @@ import os
 
 
 
-def get_path_actual (filename):
-    directorio_actual = os.path.dirname(__file__)
-    return os.path.join(directorio_actual, filename)
-# ----------------------------------------------------------------
+def get_path_actual(nombre_archivo):
+    """_summary_
 
-def read_csv (filename):
-    bikes = []
-    with open (get_path_actual(filename), "r", encoding="UTF-8") as archivo_csv:
-        contenido = csv.reader(archivo_csv, delimiter=',')
-        next(contenido)
-        for fila in contenido:
-            bikes.append(fila)
+    Args:
+        nombre_archivo (_type_): Nombre del archivo actual
+
+    Returns:
+        _type_: la ubicacion del archivo en el que se trabaja
+    """
     
-    return bikes
+    ubi = os.path.dirname(__file__)
+    
+    return os.path.join(ubi, nombre_archivo)
+
 # ----------------------------------------------------------------
-def asignar_tiempos (bikes) :
-    for bike in bikes :
-        bike[3] = random.randint(50,120)
-    return bikes
+def cargar_archivo_csv(nombre_archivo_data:str, lista):
+    """_summary_
+
+    Args:
+        nombre_archivo_data (str): Nombre del archivo de donde se obtendra la informacion
+    """
+    with open(get_path_actual(nombre_archivo_data), "r", encoding="utf-8") as archivo:
+         encabezado = archivo.readline().strip("\n").split(",")
+    
+         for linea in archivo.readlines():
+            bicicleta = {}
+            linea = linea.strip("\n").split(",")
+            id_bike, nombre, tipo, tiempo = linea
+            bicicleta["id_bike"] = int(id_bike)
+            bicicleta["nombre"] = nombre
+            bicicleta["tipo"] = tipo
+            bicicleta["tiempo"] = tiempo
+            
+            lista.append(bicicleta)
+            
+
+# -----------------------------
+
+def mostrar_ciclistas_tabla(lista:list):
+    tam = len(lista)
+    print("                      LISTA DE CICLISTAS")
+    print("ID     Nombre            Tipo        Tiempo")
+    print("------------------------------------------------------------------------")
+    for ciclista in lista:
+        print(f"{ciclista["id_bike"]}    {ciclista["nombre"]:15}    {ciclista["tipo"]:10}    {ciclista["tiempo"]}")
+
+def numeros_ramdom(lista):
+    lista["tiempo"] = random.randint(50,120)
+    return lista
 # ----------------------------------------------------------------
 
+def mapear_lista(funcion, lista:list)->list:
+    """_summary_
 
-def calcular_menor(lista:list)->int:
+    Args:
+        funcion (_type_): Funcion para mapear
+        lista (list): Lista que se va a mapear
 
+    Returns:
+        list: Se creara una lista con los datos de la lista que ingresaste y con lo que especifiques en la funcion,
+    """
+    lista_retorno = []
+    for el in lista:
+        lista_retorno.append(funcion(el))
+    return lista_retorno
+
+# ----------------------------------------------------------------
+def calcular_menor(lista: list) -> int:
+    """
+    Calcula el menor elemento de una lista.
+
+    Args:
+        lista (list): Lista de elementos numéricos.
+
+    Raises:
+        ValueError: Si la lista está vacía.
+
+    Returns:
+        int: El menor elemento de la lista.
+    """
     if len(lista) == 0:
-        raise ValueError("No esta definido el menor de una lista vacía")
+        raise ValueError("No está definido el menor de una lista vacía")
+    
     flag_primer_elemento = True
     for elemento in lista:
-        if flag_primer_elemento == True or menor > elemento:
+        if flag_primer_elemento or menor > elemento:
             flag_primer_elemento = False
             menor = elemento
 
     return menor
+# ----------------------------------------------------------------
 
-def obtener_ganador(bikes) :
-    minimo = calcular_menor(bikes) 
-    ganadores = [bike for bike in bikes if bike[3] == minimo[3]]
-    print (ganadores)
+def definir_campo(campo: str) -> str:
+    """
+    Define el campo de la lista basado en un alias corto.
 
-    if not ganadores:
-        return "No hay ganadores."
+    Args:
+        campo (str): Alias del campo a evaluar.
 
-    if(len(ganadores) == 1) :
-        return (f"El ganador es {ganadores[0][1]} con tiempo de {ganadores[0][3]}")
-    else :
-        return (f"Los ganadores son: {ganadores}")
+    Raises:
+        ValueError: Si no se ingresa un valor válido.
+
+    Returns:
+        str: El nombre completo del campo.
+    """
+    match campo:
+        case "n":
+            retorno = "nombre"
+        case "id":
+            retorno = "id_bike"
+        case "tipe":
+            retorno = "tipo"
+        case "time":
+            retorno = "tiempo"
+        case _: 
+            raise ValueError("No es un campo válido")
+    return retorno
+# ----------------------------------------------
+def swap_lista(lista:list, valor1, valor2):
+    """swap
+
+    Args:
+        lista (list): lista a swapear
+        valor1 (_type_): primer valor a swapear
+        valor2 (_type_): segundo valor a swapear
+    """
+    aux = lista[valor1]
+    lista[valor1] = lista[valor2]
+    lista[valor2] = aux
+#------------------------------------------
+def ordenar_campo(lista: list, campo: str, asc: bool = True):
+    """
+    Ordena una lista según un campo específico y en orden ascendente o descendente.
+
+    Args:
+        lista (list): Lista a ordenar.
+        campo (str): Campo a ordenar.
+        asc (bool, optional): True para orden ascendente, False para descendente. Defaults to True.
+
+    Raises:
+        ValueError: Si no se ingresa una lista.
+    """
+    if isinstance(lista, list):
+        atributo = definir_campo(campo)
+        tam = len(lista)
+        for i in range(tam - 1):
+            for j in range(i + 1, tam):
+                if (lista[i][atributo] > lista[j][atributo]) if asc else (lista[i][atributo] < lista[j][atributo]):
+                    swap_lista(lista, i, j)
+    else:
+        raise ValueError("No se ingresó ninguna lista")
+#------------------------------------------
+def asignar_ganador(lista: list):
+    """
+    Recorre la lista y asigna el ganador basado en el menor tiempo.
+
+    Args:
+        lista (list): Lista de ciclistas.
+
+    Returns:
+        list: Lista de ganadores.
+    """
+    ganador = []
+    ordenar_campo(lista, "time", True)
+    for i in lista:
+        if i["tiempo"] == lista[0]["tiempo"]:
+            ganador.append(i)
+    return ganador
+    
+# ------------------------------------------
+def filtrar_lista(funcion, lista:list)->list:
+    """filtra una lista segun la funcion
+
+    Args:
+        funcion (_type_): Funcion para filtrar
+        lista (list): Lista que se va a filtrar
+
+    Returns:
+        list: lista filtrada segun funcion
+    """
+    lista_retorno = []
+    for el in lista:
+        if funcion(el):
+            lista_retorno.append(el)
+    return lista_retorno
+
+# ----------------------------------------------------------------
+
+
+
+
+def crear_archivo_tipo(lista:list):
+    """crea un archivo csv con la lista que pasa por parametro
+
+    Args:
+        lista (list): lista con datos para crear archivo
+    """
+    
+    tipe_bike = input("Ingrese el tipo de bicicleta ").upper()
 
     
-    
-    # ganador = calcular_menor(bikes) 
-    # ganadores = [bike for bike in bikes if bike[3] == ganador[3]]
-    # if(len(ganador) == 1) :
-    #     return (f"El ganador es {ganador[0][1]} con tiempo de {ganador[0][1]}")
-    # elif(len(ganadores) > 1) :
-    #     return (f"Los ganadores son : {ganadores}")
-    # else :
-    #     return "error"
+    while tipe_bike != "BMX" and tipe_bike != "PLAYERA" and tipe_bike != "MTB" and tipe_bike != "PASEO":
+        tipe_bike = input("Ingrese un tipo de bicicleta valido: ")
+    lista_tipo = (filtrar_lista(lambda bike: bike["tipo"] == tipe_bike, lista))
     
     
-def obtener_tipos_bicicletas_sin_repetir (bikes:list) -> list:
-    tipos = [] 
-    for bike in bikes:
-        if not bike[2] in tipos:
-            tipos.append(bike[2])
+    with open(get_path_actual(tipe_bike + ".csv"), "w", encoding="utf-8") as archivo:
+        encabezado = ",".join(list(lista[0].keys())) + "\n"
+        archivo.write(encabezado)
+        for i in range(len(lista_tipo)):
+            l = ",".join(lista_tipo[i]) + "\n"
     
-    print(tipos)
-    return tipos
+        for persona in lista_tipo:
+            values = list(persona.values())
+            l = []
+            for value in values:
+                if isinstance(value,int):
+                    l.append(str(value))
+                elif isinstance(value,float):
+                    l.append(str(value))
+                else:
+                    l.append(value)
+            linea = ",".join(l) + "\n"
+            archivo.write(linea)
 
-def promedio_por_tipo(bikes) :
-    bikes_con_tiempo = asignar_tiempos(bikes)
-    promedios = []
-    for tipo in obtener_tipos_bicicletas_sin_repetir(bikes_con_tiempo) :
+# ----------------------------------------------------------------
+
+def totalizar_lista(lista:list)->int:
+    """totaliza una lista
+
+    Args:
+        lista (list): lista a sumar
+
+    Raises:
+        ValueError: no es lista
+
+    Returns:
+        int: Suma todos sus elementos
+    """
+    if  isinstance(lista, list):
         total = 0
-        for bike in bikes :
-            if(bike[2] == tipo) :
-                total += int(bike[3])
-        promedios.append((tipo, total / len(obtener_tipos_bicicletas_sin_repetir(bikes))))
-    
-    
-    return promedios
-    
+        for el in lista:
+            total += int(el)
+        return total
+    else:
+        raise ValueError("No se ingreso ninguna lista") 
 
-def filtrar_tipo(bikes, tipo) :
-    lista_filtrada_tipo = [bike for bike in bikes if bike[2] == tipo]
-    with open(f"{tipo}.csv", 'w', newline='') as file:
-        writer = csv.writer(file)
-        for bike in lista_filtrada_tipo :
-            writer.writerow(bike)
+# ----------------------------------------------------------------
+
+
+def calcular_promedio(lista:list)->int:
+    """promedia una lista
+
+    Args:
+        lista (list): lista a calcular promedio
+
+    Raises:
+        ValueError: lista vacia
+        ValueError: No es una lista
+
+    Returns:
+        float: El promedio  de la lista
+    """
+    if  isinstance(lista, list):
+        cant = len(lista)
+        if cant == 0:
+            raise ValueError("No esta definido el promedio de una lista vacia")
+        return round(totalizar_lista(lista) / cant)
+    else:
+        raise ValueError("No se ingreso ninguna lista") 
+    
+# ----------------------------------------------------------------
+
+def ordenar_por_dos_campos(lista:list, campo_uno:str, campo_dos:str):
+    """
+    Ordena una lista ascendente.
+
+    Args:
+        bicicletas (list): Lista de diccionarioss.
+        campo_tipo (str): Nombre del campo uno.
+        campo_tiempo (str): Nombre del campo 2.
+    """
+    tam = len(lista)
+    for i in range(tam - 1):
+        for j in range(i + 1, tam):
+            if lista[i][campo_uno] == lista[j][campo_uno]:
+                if lista[i][campo_dos] < lista[j][campo_dos]:
+                    lista[i], lista[j] = lista[j], lista[i]
+            elif lista[i][campo_uno] < lista[j][campo_uno]:
+                lista[i], lista[j] = lista[j], lista[i]
+
+# ----------------------------------------------------------------
+    
 
 def limpiar_pantalla ():
+    """limpia la pantalla
+
+    Returns:
+        _type_: limpia la terminal
+    """
     return system ("cls")
+# ----------------------------------------------------------------
+
 def pausar ():
+    """_summary_
+
+    Returns:
+        _type_: pausa el sistema en cada iteracion
+    """
     return system("pause")
+# ----------------------------------------------------------------
 
 def menu():
+    """crea un menu
+
+    Returns:
+        _type_: ingresa la opcion del menu
+    """
     limpiar_pantalla()
     print("   MENU DE OPCIONES : ")
-    print("1- Cargar archivo csv y mostrar ")
-    print("2- asignar tiempos aleatoriamente y mostarlos ")
-    print("3- informar el ganador")
-    print("4- informa el promedio por tipo ")
-    print("5- filtra por tipo ")
-    print("6- exit ")
+    print("1- Cargar archivo csv")
+    print("2- mostar archivo ")
+    print("3- cargar tiempos")
+    print("4- informar ganador ")
+    print("5- filtra por tipo y crea un archivo del tipo seleccionado ")
+    print("6- muestra el promedio por tipo ")
+    print("7- muestra las posiciones  ")
+    print("8- guarda las posiciones en archivos JSON  ")
+    print("9- salir del programa  ")
+    
     
     return input (" ingrese el nro de opcion que desea : ")
    
--- programa_parcial.py
+++ programa_parcial.py
@@ -1,52 +1,95 @@
 from funciones_parcial import *
+import json
 
 bikes = []
-# # 1,2) bicicletas.csv
-# csv_filename = input("ingrese el nombre del archivo a cargar : ")
-# if(isinstance(csv_filename, str)):
-#     bikes = read_csv(csv_filename)
-#     # print(bikes)
-
-# print()
-# print("--------------------------------------------------------")
-    
-# # 3) 
-# print (asignar_tiempos(bikes))
-
-# print()
-# print("--------------------------------------------------------")
-
-# # 4)
-# print (obtener_ganador(bikes))
-
-# #6)
-# print (promedio_por_tipo(bikes))
-
-
-bandera_primer_ingreso =False
+bandera_primer_ingreso =True
 bandera_segundo_ingreso = False
 seguir = "si"
+
 while seguir == "si":
     match menu():
         case "1":
-            csv_filename = input("ingrese el nombre del archivo a cargar : ")
-            if(isinstance(csv_filename, str)):
-                bikes = read_csv(csv_filename)
-                print(bikes)
-                bandera_primer_ingreso=True
+            limpiar_pantalla()
+            try:
+                nombre_archivo = input("ingrese el nombre del archivo a cargar : ")
+                cargar_archivo_csv(f"{nombre_archivo}.csv",bikes)
+                print("El archivo se cargo correctamente")
+                bandera_primer_ingreso=False
+            except:
+                print("no se encontro ningun archivo con este nombre.")
+                
         case "2":
-            print (asignar_tiempos(bikes))
-            bandera_segundo_ingreso = True
-        case "3":
-            print (obtener_ganador(bikes))
-        case "4":
-            print (promedio_por_tipo(bikes))
-        case "5":
-            print (filtrar_tipo(bikes, "playera"))
-            
-        case "6":
-            bandera_primer_ingreso =False
-            bandera_segundo_ingreso = False
-            break
+            limpiar_pantalla()
+            if bandera_primer_ingreso == False:
+                mostrar_ciclistas_tabla(bikes)
         
+        case "3":
+            if bandera_primer_ingreso == False:
+                mapear_lista(numeros_ramdom,bikes).append
+                bandera_segundo_ingreso = False
+                print("se cargaron los tiempos")
+            else :
+                print("primero debe cargar el archivo")
+        
+        case "4":
+            limpiar_pantalla()
+            if not bandera_primer_ingreso and not bandera_segundo_ingreso:
+                print("tiempo/s ganador/es")
+                mostrar_ciclistas_tabla(asignar_ganador(bikes))
+                
+            else:
+                print("Primero debe cargar el archivo y asignarle los tiempos")
+        
+        case "5":
+            limpiar_pantalla()
+            if not bandera_primer_ingreso and not bandera_segundo_ingreso:
+                crear_archivo_tipo(bikes)
+                print("Archivo creado correctamente")
+            else:
+                print("Primero debe cargar el archivo y asignarle los tiempos")
+                
+            
+        
+        case "6":
+            limpiar_pantalla()
+            if not bandera_primer_ingreso and not bandera_segundo_ingreso:
+                lista_tipos = ["BMX", "MTB", "PLAYERA"]
+                for elementos in lista_tipos:
+                    print(f"El promedio de las bicicletas {elementos} es {calcular_promedio(mapear_lista(lambda bike:bike["tiempo"],filtrar_lista(lambda tipe:tipe["tipo"] == elementos, bikes)))} minutos")
+            else:
+                print("Primero debe cargar el archivo y asignarle los tiempos")
+                
+            
+        case "7":
+            limpiar_pantalla()
+            if not bandera_primer_ingreso and not bandera_segundo_ingreso:
+                ordenar_por_dos_campos(bikes,"tipo","tiempo")
+                mostrar_ciclistas_tabla(bikes)
+            else:
+                print("Primero debe cargar el archivo y asignarle los tiempos")
+                
+        case "8":
+            limpiar_pantalla()
+            if not bandera_primer_ingreso and not bandera_segundo_ingreso:
+                ordenar_por_dos_campos(bikes, "tipo", "tiempo")
+                with open(get_path_actual("bicis.json"), "w", encoding="utf-8") as archivo:
+                    json.dump(bikes, archivo, indent=2)
+                print("Archivo JSON creado con el listado ordenado")
+            else:
+                print("Primero debe cargar el archivo y asignarle los tiempos")
+                
+        case "9":
+            limpiar_pantalla()
+            print("Se cierra el programa, muchas gracias!")
+            bandera_primer_ingreso = True
+            bandera_segundo_ingreso= True
+            break
+        case _:
+            print("Opción no válida. Intenta de nuevo.")
     pausar()
+    seguir = input("¿Deseas continuar? (si/no): ").lower()
+    if seguir == "no":
+        print("muchas gracias , que tenga buen dia!") 
+    else :
+        print("ingrese opcion valida")
+        
